44bb30f0b80a028f4802018ee3b328850630869e
Rob Christie <robchristie@gmail.com>
2010-06-21 14:00:25 -0400

    minor edit

--- app/mailers/stalker_mailer.rb
+++ app/mailers/stalker_mailer.rb
@@ -1,12 +1,10 @@
 class StalkerMailer < ActionMailer::Base
   default :from => "urlstalkers@gmail.com"
 
-def hello_email(email)
-  mail(:to => email) do |format|
-    format.text { render :text => "This is text!" }
-    format.html { render :text => "<h1>This is HTML</h1>" }
+  def hello_email(email)
+    mail(:to => email) do |format|
+      format.text { render :text => "This is text!" }
+      format.html { render :text => "<h1>This is HTML</h1>" }
+    end
   end
-end
-
-
 end
