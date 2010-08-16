f1c750c3c0735d72ff6088d21ddf63fdf6b40fce
Rob Christie <robchristie@gmail.com>
2010-08-16 16:07:36 -0400

    [#3964315] Started adding check of git sha to see if a repos has been updated since the last check. This is a quick way to see if our victim url has changed.

--- app/models/victim_url.rb
+++ app/models/victim_url.rb
@@ -18,14 +18,30 @@ class VictimUrl < ActiveRecord::Base
     agent.get(url)
     ary = agent.page.search(query)
     ary = ary.map do |e|
-      if content_only?
-        e.content
-      else
         e.to_s
-      end
     end
     ary.join("\n")
   end
+
+  def check_for_updates
+    self.publish_gist
+    if File.exist?("tmp/gists/#{self.gist_id}")
+      g = Git.open("tmp/gists/#{self.gist_id}")
+      g.remote("origin").fetch
+      g.remote("origin").merge
+    else
+      g = Git.clone("git://gist.github.com/#{self.gist_id}.git","tmp/gists/#{self.gist_id}")
+    end
+    if self.git_sha.nil?
+      self.git_sha = g.object('HEAD').sha
+      self.save
+    elsif self.git_sha != g.object('HEAD').sha
+      logger.info "Updated SHA...something has changed"
+      self.git_sha = g.object('HEAD').sha
+      self.save
+    end
+  end
+
 
   protected
 
--- lib/tasks/cron.rake
+++ lib/tasks/cron.rake
@@ -1,3 +1,4 @@
+"Run Heroku Cron to check changes in victims. muhahahah."
 task :cron => :environment do
    puts "Checking sites and sending emails"
    puts "TBD."
